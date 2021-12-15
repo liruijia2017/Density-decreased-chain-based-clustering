fc33268c32a5e56209842c4cd6ffb4057784a1eb
liruijia2017 <1142698760@qq.com>
2021-12-15 13:32:56 +0800

    Add files via upload

---
+++ DDC.m
@@ -0,0 +1,30 @@
+function Label = DDC(X,k,lambda,c)
+% Written by Ruijia Li (ruijia2017@163.com), UESTC, June 1, 2021.
+
+%% Input
+% X: data matrix (numSamp x dimSamp, dimSamp is the dimension)
+% k: used to build the mutual k-NN graph
+% lambda: used to identify core points
+% c: number of the clusters 
+%% Output
+% Label: clustering result
+
+%% Built the mutual k-NN graph
+[W,Distance] = MutualkNNGraph(X,k); 
+
+%% Calculate the density of the data & identify the local density center
+[D,LDC] = Density(W); 
+
+%% Built the density decreased chain
+[DDC,DPS,LDCD] = DensityDecreasedChain(W,D,LDC); 
+
+%% Identify Core Points
+[BP,CP] = IdentifyCorePoint(DPS,LDC,D,lambda);  
+
+%% Mine subclusters in the core points
+[PartLabel,subClusterID] = InitialSubCluster(W,LDCD,DDC,LDC,BP,CP); 
+
+%% Expand the subclusters into clusters
+Label = FinalCluster(Distance,DDC,PartLabel,LDCD,subClusterID,LDC,c);
+
+
